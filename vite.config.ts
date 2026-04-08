43bb1dec66ce5e59ad379da2f066426968c509f7
peter-tzchao <peter@fridgechannels.com>
2026-04-08 16:16:23 +0800

    更新访问

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['render-auto.fridgechannels.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
